d91efaf8fdbaf1be19d36db28cf076beb4a211d5
Nato <tom.vaidie@gmail.com>
2017-04-14 16:31:11 +0200

    Add test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world !");
+        }
+
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int divide(int a, int b)
+        {
+            return a / b;
         }
     }
 }
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(Program.add(5, 12), 17);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            Assert.AreEqual(Program.divide(5, 12), 0 );
+        }
+    }
+}
